5ecf3df833db615fef79c62fdd3c1da6ec5435a4
rodrigomoraes28 <rodrigostmoraes@gmail.com>
2024-11-05 14:39:32 -0300

    Comentários adicionados no código

--- calculadora.py
+++ calculadora.py
@@ -1,19 +1,24 @@
 # Rodrigo Sá Teles Moraes - 494145
+
+#Essa operação é a de soma
 def somar(a, b):
     return a + b
 
 print("Resultado da soma inicial:", somar(2, 3))
 
+#Essa operação é a de subtração
 def subtrair(a, b):
     return a - b
 
 print("Resultado da subtração inicial:", subtrair(4, 3))
 
+#Essa operação é a de multiplicação
 def multiplicar(a, b):
     return a * b
 
 print("Resultado da multiplicação inicial:", multiplicar(2, 5))
 
+#Essa operação é a de divisão
 def dividir(a, b):
     if b != 0:
         return a / b
@@ -23,12 +28,13 @@ def dividir(a, b):
 print("Resultado da divisão por 0:", dividir(2, 0))
 print("Resultado da divisão inteira:", dividir(6, 2))
 
+#Essa operação é a de exponenciação
 def exponenciar(a, b):
     return a ** b
 
 print("Resultado da exponenciação:", exponenciar(2, 5))
 
-
+#Essa operação é a de módulo da divisão
 def modulo(a, b):
     return a % b
 
